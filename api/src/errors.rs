6d52ddacef842f48c698ca13ea44c8b73c6a33d1
Vishnu Kumar <vishnukumart96@gmail.com>
2025-06-10 21:38:08 +0530

    feat: add errors

--- api/src/errors.rs
+++ api/src/errors.rs
@@ -1,4 +1,4 @@
-use actix_web::{http::StatusCode, HttpResponse, ResponseError};
+use actix_web::{body::BoxBody, http::StatusCode, HttpResponse, ResponseError};
 use derive_more::derive::{Display, Error};
 use serde::{Serialize, Deserialize};
 
@@ -16,12 +16,19 @@ pub struct CustomApiError{
     pub error: String,
 }
 
+impl CustomApiError{
+    pub fn internal_error() -> HttpResponse<BoxBody>{
+        let err = CustomApiError { error: String::from("Internal Server Error") };
+        HttpResponse::InternalServerError().json(err)
+    }
+}
+
 
 impl ResponseError for CustomApiError{}
 
 impl ResponseError for ApiError{
     fn error_response(&self) -> actix_web::HttpResponse<actix_web::body::BoxBody> {
-        HttpResponse::build(self.status_code()).body(self.to_string())
+        HttpResponse::build(self.status_code()).json(CustomApiError{error:self.to_string()})
     }
 
     fn status_code(&self) -> actix_web::http::StatusCode {
