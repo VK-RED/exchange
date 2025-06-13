aa659be73ae489895740bb5096eb3942ce093d36
Vishnu Kumar <vishnukumart96@gmail.com>
2025-06-13 09:43:37 +0530

    feat: add MessageType enum

--- common/src/types/order.rs
+++ common/src/types/order.rs
@@ -1,12 +1,12 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Debug, Clone, Copy, Serialize)]
+#[derive(Deserialize, Debug, Clone, Copy, Serialize, PartialEq, PartialOrd)]
 pub enum OrderSide{
     Buy,
     Sell
 }
 
-#[derive(Deserialize, Debug, Serialize)]
+#[derive(Deserialize, Debug, Serialize, Clone)]
 pub struct Order {
     pub id: String,
     pub user_id: String,
@@ -14,4 +14,15 @@ pub struct Order {
     pub market: String,
     pub price: u128,
     pub quantity: u16,
+}
+
+impl PartialEq for Order {
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id
+    }
+}
+
+#[derive(Deserialize, Debug, Clone, Serialize, PartialEq)]
+pub enum MessageType{
+    CreateOrder(Order)
 }
