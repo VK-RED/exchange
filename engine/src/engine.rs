91b55c386be4457099f47d398622aebdaa7693d9
Vishnu Kumar <vishnukumart96@gmail.com>
2025-06-13 20:54:15 +0530

    refactor: engine and orderbook

--- engine/src/engine.rs
+++ engine/src/engine.rs
@@ -1,26 +1,36 @@
 use std::{collections::HashMap, sync::mpsc};
 
 use common::types::order::MessageType;
-use r2d2_redis::{r2d2::Pool, RedisConnectionManager};
+use r2d2_redis::{r2d2::{self, Pool}, RedisConnectionManager};
 
 use crate::orderbook::OrderBook;
 
-pub type UserBalance = HashMap<String, u128>;
 pub type MarketTx = mpsc::Sender<MessageType>;
+
+#[derive(Clone, Debug)]
+pub struct AssetBalance{
+    pub available_amount: u128,
+    pub locked_amount: u128,
+}
+
+pub type UserAssetBalance = HashMap<String, HashMap<String, AssetBalance>>;
 
 #[derive(Clone)]
 pub struct Engine {
     pub orderbooks: Vec<OrderBook>,
-    pub user_balances: HashMap<String, UserBalance>,
+    pub user_balances: UserAssetBalance,
     pub redis_pool: Pool<RedisConnectionManager>,
     pub order_queue_key: String,
 }
+
 
 impl Engine {
 
     // TODO: RECOVER SNAPSHOT 
     // AND PERIODICALLY SAVE THE SNAPSHOT
-    pub fn init(redis_pool: Pool<RedisConnectionManager>) -> Self{
+    pub fn init() -> Self{
+
+        let redis_pool = Self::init_redis_pool();
 
         let markets = [
             "SOL_USDC".to_string(),
@@ -35,7 +45,7 @@ impl Engine {
         }
 
         // Initially all the balances will be zero
-        let balances: HashMap<String, UserBalance> = HashMap::new();
+        let balances: UserAssetBalance = HashMap::new();
 
         // should be same as what we push in the queue in api
         let order_queue_key = "orders".to_string();
@@ -48,6 +58,13 @@ impl Engine {
         }
     }
 
+    pub fn init_redis_pool() -> Pool<RedisConnectionManager>{
+        let redis_url = std::env::var("REDIS_URL").unwrap_or_else(|_e|String::from("redis://127.0.0.1:6379"));
+        let manager = RedisConnectionManager::new(redis_url).expect("Failed to create redis manager");
+        let pool = r2d2::Pool::builder().build(manager).expect("Failed to create Redis Pool");
+        pool
+    }
+
 
     pub fn init_market_tx() -> HashMap<String, MarketTx>{
         HashMap::new()
@@ -57,4 +74,6 @@ impl Engine {
         let deserialized = serde_json::from_str::<MessageType>(message);
         deserialized   
     }
+
+    
 }
--- engine/src/orderbook.rs
+++ engine/src/orderbook.rs
@@ -1,15 +1,17 @@
 use std::{sync::{Arc, Mutex}};
 
 use common::types::order::{MessageType, Order, OrderSide};
-use r2d2_redis::redis::Commands;
-use crate::engine::Engine;
+use r2d2_redis::{r2d2::{Pool}, redis::Commands, RedisConnectionManager};
+use crate::engine::UserAssetBalance;
+
+pub type RedisResponse = Result<(), r2d2_redis::redis::RedisError>;
 
 #[derive(Debug, Clone)]
 pub struct OrderBook {
     pub market: String,
     pub bids: Vec<Order>,    
     pub asks: Vec<Order>,
-    pub current_price: u128,
+    pub last_price: u128,
 }
 
 impl OrderBook {
@@ -17,58 +19,47 @@ impl OrderBook {
     pub fn new(market:String) -> Self {
         Self { 
             market, 
-            current_price:0,
+            last_price:0,
             bids: vec![], 
             asks:vec![]
         }
     }
 
-    pub fn add_order(&mut self, order:Order) -> usize {
+    pub fn add_order(&mut self, order:Order){
         let orders = match order.side {
             OrderSide::Buy => &mut self.bids,
             OrderSide::Sell => &mut self.asks,
         }; 
         orders.push(order);
-        orders.len()
     }
 
-    pub fn process_order(message_type:MessageType, engine: Arc<Mutex<Engine>>){
+    pub fn process(
+            &mut self, 
+            message_type:MessageType, 
+            user_balances:Arc<Mutex<UserAssetBalance>>,
+            pool:&Pool<RedisConnectionManager>
+    ){
+        let mut conn = pool.get().unwrap();
 
-        let mut guard = engine.lock().unwrap();
-        let pool = &guard.redis_pool;
-        let conn = pool.get();
+        match message_type {
 
-        if conn.is_err(){
-            println!("error while establising redis connection from orderbook");
+            MessageType::CreateOrder(order) => {
+
+                let order_id = order.id.clone();
+
+                self.add_order(order);
+
+                let publish_message = "Order completed Successfully";
+                let redis_response:RedisResponse = conn.publish(&order_id, publish_message);
+
+                println!("published message to order id : {}", order_id);
+
+                if let Err(e) = redis_response {
+                    println!("Error while publishing to the order id : {}", e);
+                }
+            },
         }
-        else{
-            let mut conn = conn.unwrap();
-            
-            match message_type {
-                MessageType::CreateOrder(order) => {
-                    let message = "Order processed successfully";
-                    let ob = &guard.orderbooks.iter().find(|orderbook| orderbook.market == order.market);
 
-                    if ob.is_none(){
-                        let message = format!("No orderbook found for the market : {}", &order.market);
-                        let r: Result<(), r2d2_redis::redis::RedisError> = conn.publish(&order.id, message);
-                        if r.is_err(){
-                            println!("eee : {}", r.unwrap_err());
-                        }
-                    }
-
-                    let res: Result<(), r2d2_redis::redis::RedisError> = conn.publish(&order.id, message);
-                    if res.is_err(){
-                        let err = res.unwrap_err();
-                        println!("ERROR WHILE PUBLISING TO PUB SUB in market : {}, err: {}", &order.market, err);
-                    }
-                
-                },
-            }
-
-        }
-        
     }
-
 
 }
