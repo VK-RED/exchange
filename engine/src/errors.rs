afdae746c4a07b132321444d9a186588b005675f
Vishnu Kumar <vishnukumart96@gmail.com>
2025-06-26 07:28:12 +0530

    feat: refactor EngineError using thiserror

--- engine/src/errors.rs
+++ engine/src/errors.rs
@@ -1,16 +1,31 @@
-#[derive(Debug)]
+use common::types::error::ErrorResponse;
+use serde::Serialize;
+use thiserror::Error;
+
+#[derive(Debug, Error, Serialize)]
 pub enum EngineError {
-    OrderBookError(OrderBookError),
-    BalanceError(BalanceError),
-}
-
-#[derive(Debug)]
-pub enum OrderBookError{
+    #[error("User Not Found ! Please Signup !")]
     UserNotFound,
-    ExecuteMarketOrder,
+    #[error("The order can't be executed, as it will be filled partially !")]
+    PartialOrderFill,
+    #[error("Given user does not have permission to perform the action")]
+    MismatchUser,
+    #[error("Enter valid order_id")]
+    InvalidOrderId,
+    #[error("Internal Server Error")]
+    InternalError,
+    #[error("User does not have sufficient balance")]
+    InsufficientBalance
 }
 
-#[derive(Debug)]
-pub enum BalanceError {
-    InsufficientBalance
+impl EngineError {
+    pub fn to_error_response(self:Self) -> ErrorResponse{
+        let error_code = serde_json::to_string(&self).unwrap_or_else(|_|"INTERNAL_ERROR".to_string());
+        let error_message = self.to_string();
+
+        ErrorResponse {
+            code: error_code,
+            message: error_message
+        }
+    }
 }
