779bd1f1b1c1de61f28b8bbe2d27495b8749c204
Vishnu Kumar <vishnukumart96@gmail.com>
2025-06-27 16:50:59 +0530

    feat: add get_depth handler

--- api/src/entrypoint.rs
+++ api/src/entrypoint.rs
@@ -27,6 +27,7 @@ macro_rules! init_app {
                 .service(crate::handlers::order::cancel::cancel_order)
                 .service(crate::handlers::order::cancel_all::cancel_all_orders)
                 .service(crate::handlers::order::open_orders::get_all_open_orders)
+                .service(crate::handlers::depth::get_depth)
             )
     };
 }
