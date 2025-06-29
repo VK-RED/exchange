6d41af0e79f4b0d83e1b429b7bcddfe399a3eeab
Vishnu Kumar <vishnukumart96@gmail.com>
2025-06-29 12:41:52 +0530

    feat: add redis service

--- db_filler/src/main.rs
+++ db_filler/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use dotenv::dotenv;
+
+use crate::services::redis::RedisService;
+
+mod services;
+
+#[tokio::main]
+async fn main() {
+
+    dotenv().ok();
+
+    let mut redis_service = RedisService::new().await;
+
+    println!("starting db_filler");
+
+    loop {
+        let message = redis_service.get_message_from_engine().await;
+        println!("recieved message from engine : {:?}", message );
+    
+    }
+    
 }
