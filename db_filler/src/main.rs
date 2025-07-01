8e28e74bf0ab5e8843842cac79371f188608ae45
Vishnu Kumar <vishnukumart96@gmail.com>
2025-07-01 13:47:40 +0530

    feat: add process message

--- db_filler/src/main.rs
+++ db_filler/src/main.rs
@@ -1,6 +1,6 @@
 use dotenv::dotenv;
 
-use crate::services::redis::RedisService;
+use crate::services::{db::DbManager, redis::RedisService};
 
 mod services;
 
@@ -10,13 +10,16 @@ async fn main() {
     dotenv().ok();
 
     let mut redis_service = RedisService::new().await;
+    let db_manager = DbManager::new().await;
 
     println!("starting db_filler");
 
     loop {
         let message = redis_service.get_message_from_engine().await;
-        println!("recieved message from engine : {:?}", message );
-    
+        if let Some(msg) = message {
+            println!("received msg : {}", msg);
+            db_manager.process_message(&msg).await;
+        }        
     }
     
 }
