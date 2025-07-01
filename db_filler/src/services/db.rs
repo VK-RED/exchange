73003c5db46c26ed8ce69401f7bb1a473e199e16
Vishnu Kumar <vishnukumart96@gmail.com>
2025-07-01 13:47:10 +0530

    feat: add db method for each messageType

--- db_filler/src/services/db.rs
+++ db_filler/src/services/db.rs
@@ -1,15 +1,16 @@
 use common::message::db_filler::DbFillerMessage;
 use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
+use store::{Order, UpdateDbOrder};
 
 pub struct DbManager{
-    conn: Pool<Postgres>
+    pool: Pool<Postgres>
 }
 
 impl DbManager{
     pub async fn new() -> Self{
         let db_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
 
-        let conn = PgPoolOptions::new().
+        let pool = PgPoolOptions::new().
         max_connections(5)
         .connect(&db_url)
         .await
@@ -19,7 +20,7 @@ impl DbManager{
         println!("DB connection successfull");
 
         Self {
-            conn
+            pool
         }
     }
 
@@ -41,12 +42,86 @@ impl DbManager{
                 update_orders 
             } => {
 
+                let mut add_res = None;
+                let update_res;
+
+                let update_db_orders: Vec<UpdateDbOrder> = update_orders.into_iter().map(|order|{
+
+                    let status = serde_json::to_string(&order.status).unwrap_or_default();
+                    UpdateDbOrder { 
+                        order_id: order.order_id, 
+                        filled_quantity:order.filled_quantity.to_string(), 
+                        status: status 
+                    }
+                }).collect();
+
+                if let Some(order) = add_order {
+
+                    let status = serde_json::to_string(&order.status).unwrap_or_default();
+                    let side = serde_json::to_string(&order.side).unwrap_or_default();
+
+                    let parsed_order = Order{
+                        filled_quantity: order.filled_quantity.to_string(),
+                        id: order.order_id,
+                        order_status: status,
+                        price: order.price.to_string(),
+                        quantity: order.quantity.to_string(),
+                        side,
+                    };
+
+                    let res = Order::add_order(parsed_order, &self.pool);
+                    add_res = Some(res);
+                }
+                else{
+                    println!("add_order is NONE, only update orders will be executed");
+                }
+
+                update_res = Order::update_orders(update_db_orders, &self.pool);
+
+
+                if let Some(val) = add_res {
+                    let res = val.await;
+
+                    if let Err(e) = res {
+                        println!("error while adding order to db : {}", e);
+                    }
+                } 
+
+                if let Err(e) = update_res.await {
+                    println!("error while updating orders: {}", e);
+                }
+
             },
             DbFillerMessage::AddTrade(trades) => {
+
+                let parsed_trades: Vec<store::Trade> = trades.into_iter().map(|trade| {
+
+                    let add_trade = store::Trade {
+                        id : i64::from(trade.id),
+                        market: trade.market,
+                        matched_at: i64::from(trade.timestamp),
+                        price: trade.price.to_string(),
+                        quantity: trade.quantity.to_string(),
+                        quote_qty: trade.quote_qty.to_string(),
+                    };
+
+                    add_trade
+
+                }).collect();
+
+                let res = store::Trade::add_trades(parsed_trades, &self.pool).await;
+
+                if let Err(e) = res{
+                    println!("Error while adding trade : {}", e);
+                }
 
             },
             DbFillerMessage::UpdateCancelOrders(orders) => {
 
+                let res = Order::update_cancelled_orders(orders, &self.pool).await;
+                if let Err(e) = res {
+                    println!("Error while cancelling orders: {}", e);
+                }
             }
         }
 
