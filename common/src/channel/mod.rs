cce056e97047150b06be18af7875ba6394f5afa7
Vishnu Kumar <vishnukumart96@gmail.com>
2025-07-08 21:36:40 +0530

    feat: add user channel

--- common/src/channel/mod.rs
+++ common/src/channel/mod.rs
@@ -4,3 +4,6 @@ pub const ORDER_CHANNEL: &'static str = "orders";
 
 // db updated from db_filler are pushed in this channel
 pub const DB_CHANNEL:&'static str = "db_filler";
+
+// user queries are pushed in this channel
+pub const USER_CHANNEL: &'static str = "user";
