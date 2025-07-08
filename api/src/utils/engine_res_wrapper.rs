2f5670bc6d431d98d0c723fe19f4278edaa9f288
Vishnu Kumar <vishnukumart96@gmail.com>
2025-07-08 21:41:34 +0530

    feat: add get_user_engine_http_response

--- api/src/utils/engine_res_wrapper.rs
+++ api/src/utils/engine_res_wrapper.rs
@@ -1,5 +1,5 @@
 use actix_web::{HttpResponse, ResponseError};
-use common::{message::api::MessageFromApi, types::error::ErrorResponse};
+use common::{message::api::{MessageFromApi, UserMessageFromApi}, types::error::ErrorResponse};
 use serde::{de::DeserializeOwned, Serialize};
 
 use crate::services::redis::{PubSubService, RedisService};
@@ -19,6 +19,46 @@ pub fn get_engine_http_response<T:DeserializeOwned+Serialize>(
     }
 
     if let Err(e) = redis_service.publish_message_to_engine(message_from_api){
+        return e.error_response();
+    }
+
+    let message = match pub_sub_service.get_message_from_engine() {
+        Ok(msg) => msg,
+        Err(e) => return e.error_response(),
+    };
+
+    if let Err(e) = pub_sub_service.unsubscribe(){
+        return e.error_response();
+    }
+
+    let deserialized: Result<MessageResult<T>, serde_json::Error> = serde_json::from_str(&message);
+
+    match deserialized {
+        Ok(res) => {
+            match res {
+                Ok(res) => HttpResponse::Ok().json(res),
+                Err(e) => HttpResponse::BadRequest().json(e)
+            }
+        },
+        Err(e) =>{
+            println!("deserial error : {:?}", e);
+            e.error_response()   
+        } 
+    }
+}
+
+pub fn get_user_engine_http_response<T:DeserializeOwned+Serialize>(
+    channel:String,
+    message_from_api: UserMessageFromApi,
+    redis_service: &mut RedisService,
+    pub_sub_service: &mut PubSubService,
+) -> HttpResponse {
+
+    if let Err(e) = pub_sub_service.subscribe_to_channel(&channel) {
+        return e.error_response();
+    }
+
+    if let Err(e) = redis_service.publish_user_message_to_engine(message_from_api){
         return e.error_response();
     }
 
