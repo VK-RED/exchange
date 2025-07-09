f76fbdbb06238e9c8e89c15634dd9ba7baa8de89
Vishnu Kumar <vishnukumart96@gmail.com>
2025-07-09 21:04:29 +0530

    feat: remove redis conn in engine

--- engine/src/engine.rs
+++ engine/src/engine.rs
@@ -1,7 +1,5 @@
 use std::{collections::HashMap, sync::{mpsc, Arc, Mutex}};
-
 use common::message::api::MessageFromApi;
-use r2d2_redis::{r2d2::{self, Pool}, RedisConnectionManager};
 
 use crate::orderbook::OrderBook;
 
@@ -14,7 +12,6 @@ pub type UserAssetBalance = HashMap<String, HashMap<String, AssetBalance>>;
 pub struct Engine {
     pub orderbooks: Vec<OrderBook>,
     pub user_balances: UserAssetBalance,
-    pub redis_pool: Pool<RedisConnectionManager>,
 }
 
 
@@ -23,8 +20,6 @@ impl Engine {
     // TODO: RECOVER SNAPSHOT 
     // AND PERIODICALLY SAVE THE SNAPSHOT
     pub fn init() -> Self{
-
-        let redis_pool = Self::init_redis_pool();
 
         let assets = [
             ("SOL".to_string(),9),
@@ -43,8 +38,7 @@ impl Engine {
 
         Self { 
             orderbooks, 
-            user_balances: balances,
-            redis_pool, 
+            user_balances: balances, 
         }
     }
 
@@ -102,14 +96,6 @@ impl Engine {
 
         
     }
-
-    pub fn init_redis_pool() -> Pool<RedisConnectionManager>{
-        let redis_url = std::env::var("REDIS_URL").unwrap_or_else(|_e|String::from("redis://127.0.0.1:6379"));
-        let manager = RedisConnectionManager::new(redis_url).expect("Failed to create redis manager");
-        let pool = r2d2::Pool::builder().build(manager).expect("Failed to create Redis Pool");
-        pool
-    }
-
 
     pub fn init_market_tx() -> HashMap<String, MarketTx>{
         HashMap::new()
